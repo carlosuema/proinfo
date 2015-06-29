e46e49c0b38a40792d1e856141d6587202a77025
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2015-06-29 14:50:58 -0300

    Atualizado script de auto-instalação do tema padrão do Lubuntu.

--- 71-2010/jessie/setup-lubuntu-artwork.sh
+++ 71-2010/jessie/setup-lubuntu-artwork.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-apt install --no-install-recommends gtk2-engines-pixbuf
+do_apt() {
+  apt install -y --no-install-recommends ${@}
+}
+
+do_apt gtk2-engines-pixbuf
 
 wget -c https://copy.com/R20vwNTRwu21kFiG/boxtheme.tar.gz?download=1 -O boxtheme.tar.gz
 wget -c https://copy.com/6HXRIlh0skL9e27S/boxicons.tar.gz?download=1 -O boxicons.tar.gz
@@ -13,6 +17,11 @@ wget -c http://bazaar.launchpad.net/~lubuntu-art/lubuntu-artwork/quantal/downloa
 tar xzvf boxtheme.tar.gz -C /usr/local/share/themes
 tar xzvf boxicons.tar.gz -C /usr/local/share/icons
 install -m 644 lubuntu-logo.png /usr/local/share/lubuntu/images
+
+for theme in /usr/local/share/icons/*
+do
+    gtk-update-icon-cache -ftq ${theme}
+done
 
 for theme in /usr/local/share/themes/*
 do
