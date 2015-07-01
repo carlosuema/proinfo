988afd212d56de559d190e38192f5ac05a5d1bcd
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2015-07-01 13:43:23 -0300

    Corrigido comando para atualiar o cache de ícones nos scripts de instalação de temas.

--- 71-2010/jessie/setup-numix-artwork.sh
+++ 71-2010/jessie/setup-numix-artwork.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-do_apt() {
-  apt install -y --no-install-recommends $@
-}
-
-do_apt libgtk2.0-bin libgtk-3-bin
+do_apt gtk2-engines-murrine
 
 [ -d /usr/local/share/themes ] || mkdir -p /usr/local/share/themes
 [ -d /usr/local/share/icons ] || mkdir -p /usr/local/share/icons
@@ -18,16 +14,15 @@ do_apt libgtk2.0-bin libgtk-3-bin
   cd /usr/local/share/icons
   git clone git://github.com/numixproject/numix-icon-theme .numix-icon-theme
   git clone git://github.com/numixproject/numix-icon-theme-circle .numix-icon-theme-circle
-  ln -s .numix-icon-theme/Numix .
-  ln -s .numix-icon-theme/Numix-Light .
-  ln -s .numix-icon-theme-circle/Numix-Circle .
-  ln -s .numix-icon-theme-circle/Numix-Circle-Light .
+  ln -sf .numix-icon-theme/Numix .
+  ln -sf .numix-icon-theme/Numix-Light .
+  ln -sf .numix-icon-theme-circle/Numix-Circle .
+  ln -sf .numix-icon-theme-circle/Numix-Circle-Light .
 )
 
 for theme in /usr/local/share/icons/*
 do
-    gtk-update-icon-cache -ftq ${theme}
-    gtk-update-icon-cache-3.0 -ftq ${theme}
+    update-icon-caches ${theme}
 done
 
 for theme in /usr/local/share/themes/*
