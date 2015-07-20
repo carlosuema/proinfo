6d9f4bc259fc532eefc57c4260e1923b209b4786
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2015-07-20 13:07:48 -0300

    Adicionado comando "apt update" ao script setup-iceweasel-release.sh.

--- setup-iceweasel-release.sh
+++ setup-iceweasel-release.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 wget http://mozilla.debian.net/pkg-mozilla-archive-keyring_1.1_all.deb -O /tmp && dpkg -i /tmp/pkg-mozilla-archive-keyring_1.1_all.deb
 install -m 644 etc/apt/sources.list.d/debian-mozilla-team-iceweasel-release.list /etc/apt/sources.list.d
+apt update
