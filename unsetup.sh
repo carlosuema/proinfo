1e24fee48fdefce0113216ac761561effe0b74e7
Laércio Benedito Sivali de Sousa <laerciosousa@se-pmmc.com.br>
2015-08-07 17:06:37 -0300

    Generalizado o arquivo unsetup.sh para contemplar o caso de 5 terminais.

--- unsetup.sh
+++ unsetup.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-tn502_endereco="$(lspci | grep SM501 | cut -d' ' -f1 | sed 's/\./:/')"
-tn502_display=":$(echo ${tn502_endereco} | awk -F: '{ print $1 * 100 + $2 * 10 + $3 }')"
+get_display () {
+    echo ${1} | awk -F: '{ print $1 * 100 + $2 * 10 + $3 }'
+}
 
-systemctl disable x-daemon-3seats@${tn502_display}.service
+tn502_endereco=($(lspci | grep SM501 | cut -d' ' -f1 | sed 's/\./:/'))
+tn502_display=":$(( $(get_display ${tn502_endereco[0]) + $(get_display ${tn502_endereco[1]) ))"
 
-rm -f /etc/udev/rules.d/71-usb-*seats-*hub*.rules    \
-      /etc/udev/rules.d/72-usb-*seats-late.rules     \
-      /etc/X11/xorg.conf.d/tn502-*seats.conf         \
-      /etc/X11/xorg.conf.d/nested-3seats.conf        \
-      /etc/lightdm/lightdm.conf.d/logind.conf        \
-      /etc/lightdm/lightdm.conf.d/xephyr-*seats.conf \
-      /etc/systemd/scripts/x-daemon                  \
-      /etc/systemd/system/x-daemon-3seats@.service
+systemctl disable x-daemon-Nseats@${tn502_display}.service
+
+rm -r -f /etc/udev/rules.d/71-usb-*seats*.rules         \
+         /etc/udev/rules.d/72-usb-*seats-late.rules     \
+         /etc/X11/xorg.conf.d/tn502-*seats.conf         \
+         /etc/X11/xorg.conf.d/nested-*seats.conf        \
+         /etc/lightdm/lightdm.conf.d/logind.conf        \
+         /etc/lightdm/lightdm.conf.d/xephyr-*seats.conf \
+         /etc/systemd/scripts/x-daemon                  \
+         /etc/systemd/system/x-daemon-Nseats@.service   \
+         /etc/systemd/system/x-daemon-Nseats@.service.d
 
 echo "Reinicie o computador para finalizar a remoção das configurações."
