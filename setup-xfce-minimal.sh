fad51e66f3eb4dcc8bc566a8f1fec991400dbeb8
Laércio de Sousa <laercio@proinfo0.egruppi.lan>
2015-08-21 11:23:49 -0300

    Incluído pacote para configurar o tema padrão do cursor do mouse na instalação mínima do XFCE.

--- setup-xfce-minimal.sh
+++ setup-xfce-minimal.sh
@@ -3,7 +3,7 @@
 pacotes_base="desktop-base"
 pacotes_xorg="xorg"
 pacotes_lightdm="lightdm"
-pacotes_xfce="xfce4 tango-icon-theme thunar-volman gtk3-engines-xfce xfce4-whiskermenu-plugin mousepad ristretto thunar-archive-plugin thunar-media-tags-plugin xfburn xfce4-artwork xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-places-plugin xfce4-quicklauncher-plugin xfce4-systemload-plugin xfce4-taskmanager xfce4-terminal xfce4-volumed evince-gtk"
+pacotes_xfce="xfce4 tango-icon-theme thunar-volman gtk3-engines-xfce xfce4-whiskermenu-plugin mousepad ristretto thunar-archive-plugin thunar-media-tags-plugin xfburn xfce4-artwork xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-places-plugin xfce4-quicklauncher-plugin xfce4-systemload-plugin xfce4-taskmanager xfce4-terminal xfce4-volumed evince-gtk gnome-themes-standard-data"
 pacotes_gvfs="gvfs-backends gvfs-fuse ntfs-3g dosfstools"
 pacotes_audio="pulseaudio pulseaudio-module-x11 rtkit"
 
