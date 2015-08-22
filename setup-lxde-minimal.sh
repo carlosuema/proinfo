0356e5c1b417e8010135655dcce27540d7e6d7d7
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2015-08-22 12:30:38 -0300

    Unificados os scripts para instalação mínima do LXDE.

---
+++ setup-lxde-minimal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pacotes_base="desktop-base"
+pacotes_xorg="xorg dbus-x11"
+pacotes_lightdm="lightdm"
+pacotes_lxde="lxde lxtask gtk2-engines notification-daemon compton evince-gtk"
+pacotes_gvfs="gvfs-backends gvfs-fuse ntfs-3g dosfstools"
+pacotes_audio="pulseaudio pulseaudio-module-x11 rtkit"
+
+do_apt() {
+  apt install -y --no-install-recommends ${@}
+}
+
+install -m 644 etc/apt/sources.list /etc/apt
+
+apt update
+apt -y upgrade
+
+do_apt ${pacotes_base} ${pacotes_xorg} ${pacotes_lightdm} ${pacotes_lxde} ${pacotes_gvfs} ${pacotes_audio}
