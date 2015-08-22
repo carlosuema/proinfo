b6ed00acd5a3b4ded69aa967416ebe2170562bd2
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2015-08-22 09:25:29 -0300

    Adicionado script para instalação mínima do XFCE mais recente (jessie-backports).

---
+++ setup-xfce-backport-minimal.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pacotes_base="desktop-base"
+pacotes_xorg="xorg"
+pacotes_lightdm="lightdm"
+pacotes_xfce="xfce4 tango-icon-theme thunar-volman gtk3-engines-xfce xfce4-whiskermenu-plugin mousepad ristretto thunar-archive-plugin thunar-media-tags-plugin xfburn xfce4-artwork xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-places-plugin xfce4-quicklauncher-plugin xfce4-systemload-plugin xfce4-taskmanager xfce4-terminal xfce4-volumed evince-gtk gnome-themes-standard-data"
+pacotes_gvfs="gvfs-backends gvfs-fuse ntfs-3g dosfstools"
+pacotes_audio="pulseaudio pulseaudio-module-x11 rtkit"
+
+do_apt() {
+  apt install -y --no-install-recommends ${@}
+}
+
+install -m 644 etc/apt/sources.list /etc/apt
+install -m 644 etc/apt/sources.list.d/obs-home-lbssousa-debian.list /etc/apt/sources.list.d
+install -m 644 etc/apt/sources.list.d/obs-home-lbssousa-debian-xfce.list /etc/apt/sources.list.d
+
+apt-key add etc/apt/sources.list.d/obs-home-lbssousa.key
+
+apt update
+apt -y upgrade
+
+do_apt ${pacotes_base} ${pacotes_xorg} ${pacotes_lightdm} ${pacotes_xfce} ${pacotes_gvfs} ${pacotes_audio}
