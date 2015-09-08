443e87e94958479126bb6ac6d0c68f0407b0fa41
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2015-09-08 16:21:56 -0300

    Corrigido script unsetup.sh.

--- unsetup.sh
+++ unsetup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-tn502_endereco=($(lspci | grep SM501 | cut -d' ' -f1 | sed 's/\./:/'))
-
 systemctl disable x-daemon-Nseats.service
-<<<<<<< HEAD
 systemctl disable xorg@0.socket
-=======
->>>>>>> 59f7bb949db28b8d1ef87bedeb976b3614466232
 
 rm -r -f /etc/udev/rules.d/71-usb-*seats*.rules         \
          /etc/udev/rules.d/72-usb-*seats-late.rules     \
@@ -15,15 +10,10 @@ rm -r -f /etc/udev/rules.d/71-usb-*seats*.rules         \
          /etc/lightdm/lightdm.conf.d/logind.conf        \
          /etc/lightdm/lightdm.conf.d/xephyr-*seats.conf \
          /etc/systemd/scripts/x-daemon                  \
-<<<<<<< HEAD
          /etc/systemd/system/x-daemon-Nseats.service    \
          /etc/systemd/system/x-daemon-Nseats.service.d  \
          /etc/systemd/system/xorg@.service              \
          /etc/systemd/system/xorg@.service.d            \
          /etc/systemd/system/xorg@.socket
-=======
-         /etc/systemd/system/x-daemon-Nseats.service   \
-         /etc/systemd/system/x-daemon-Nseats.service.d
->>>>>>> 59f7bb949db28b8d1ef87bedeb976b3614466232
 
 echo "Reinicie o computador para finalizar a remoção das configurações."
