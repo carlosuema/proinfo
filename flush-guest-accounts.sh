68fcc04dc016d23037f05f69e8940487dcaab966
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2015-09-16 14:56:34 -0300

    Atualizado o script flush-guest-accounts.sh para remover explicitamente os grupos.

--- flush-guest-accounts.sh
+++ flush-guest-accounts.sh
@@ -3,3 +3,8 @@ for i in $(getent passwd | cut -d: -f1 | grep guest | grep -v guest-template)
 do
     deluser --system $i
 done
+
+for j in $(getent group | cut -d: -f1 | grep guest | grep -v guest-template)
+do
+    delgroup --system $j
+done
